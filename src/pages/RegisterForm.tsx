ed91d217d40683c00379a71e95bfaed0b9cb1a24
Dmitrii Cetvericov <d.cetvericov@live.de>
2025-06-29 17:15:53 +0200

    The form is kinda ready to be integrated to the new API

--- src/pages/RegisterForm.tsx
+++ src/pages/RegisterForm.tsx
@@ -1,3 +1,74 @@
+import {useState} from "react";
+import * as React from "react";
+
+interface RegisterData {
+    username: string;
+    password: string;
+    passwordRepeated: string;
+}
+
 export default function RegisterForm() {
-    return <div className="m-3">Register</div>
+    const [registerData, setRegisterData] = useState<RegisterData>(
+        {
+            username: '',
+            password: '',
+            passwordRepeated: ''
+        }
+    );
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setRegisterData(
+            {
+                ...registerData,
+                [name]: value
+            }
+        );
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // Handle form submission
+        console.log('Form submitted:', registerData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="m-3">
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    value={registerData.username}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    value={registerData.password}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="passwordRepeated" className="form-label">Password (repeated)</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    id="passwordRepeated"
+                    name="passwordRepeated"
+                    value={registerData.passwordRepeated}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit" className="btn btn-primary">Register</button>
+        </form>
+    )
 }
