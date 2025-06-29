5b311eefd6c85afd2a6c1ee214a877a74e913b0c
Dmitrii Cetvericov <d.cetvericov@live.de>
2025-06-29 18:42:05 +0200

    I think I'm happy with the form, begin to add the request

--- src/pages/RegisterForm.tsx
+++ src/pages/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {type FormEvent, useState} from "react";
 import * as React from "react";
 
 interface RegisterData {
@@ -16,6 +16,8 @@ export default function RegisterForm() {
         }
     );
 
+    const [status, setStatus] = useState<string | null>(null);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target;
         setRegisterData(
@@ -32,43 +34,61 @@ export default function RegisterForm() {
         console.log('Form submitted:', registerData);
     };
 
-    return (
-        <form onSubmit={handleSubmit} className="m-3">
-            <div className="mb-3">
-                <label htmlFor="username" className="form-label">Username</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    name="username"
-                    value={registerData.username}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    value={registerData.password}
-                    onChange={handleChange}
-                />
-            </div>
+    const determineButtonClass = () => {
+        if (!registerData.username || !registerData.password || !registerData.passwordRepeated) {
+            return "btn btn-primary disabled";
+        }
 
-            <div className="mb-3">
-                <label htmlFor="passwordRepeated" className="form-label">Password (repeated)</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="passwordRepeated"
-                    name="passwordRepeated"
-                    value={registerData.passwordRepeated}
-                    onChange={handleChange}
-                />
-            </div>
-            <button type="submit" className="btn btn-primary">Register</button>
-        </form>
+        return "btn btn-primary";
+    }
+
+    const statusOrNothing = () => {
+        if (status) {
+            return <div className="alert">{status}</div>
+        }
+        return null;
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="m-3">
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        name="username"
+                        value={registerData.username}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        value={registerData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="passwordRepeated" className="form-label">Password (repeated)</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="passwordRepeated"
+                        name="passwordRepeated"
+                        value={registerData.passwordRepeated}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit" className={determineButtonClass()}>Register</button>
+            </form>
+            {statusOrNothing()}
+        </>
     )
 }
